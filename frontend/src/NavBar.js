6b329a56f131ed3a083b0700fe7caa39ea384a70
Sean Gibson <seanthomasgibson@gmail.com>
2021-06-01 16:54:18 -0700

    Made navigation bar non-collapsable. Modified NavBar() in frontend/src/NavBar.js.

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -5,34 +5,32 @@ import Nav from "react-bootstrap/Nav";
 
 /**
  * Component for navigation bar
+ * @param {Object} param0 
  * @returns JSX code for rendering navigation bar
  */
-const NavBar = () => {
+const NavBar = ({ logout }) => {
   const username = useSelector(store => store.username);
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" varian="light">
       <Navbar.Brand href="/">Travel Activities</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        {
-          username ? (
-            <Nav className="mr-auto">
-              <Nav.Link href="/favorite-activities">
-                My Favorite Activities
-              </Nav.Link>
-              <Nav.Link href="/create-plan">Create a New Plan</Nav.Link>
-              <Nav.Link href="/plans">My Plans</Nav.Link>
-              <Nav.Link href="/">Logout</Nav.Link>
-            </Nav>
-          ) : (
-            <Nav className="mr-auto">
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-              <Nav.Link href="/login">Log In</Nav.Link>
-            </Nav>
-          )
-        }
-      </Navbar.Collapse>
+      {
+        username ? (
+          <Nav className="ml-auto">
+            <Nav.Link href="/favorite-activities">
+              My Favorite Activities
+            </Nav.Link>
+            <Nav.Link href="/create-plan">Create a New Plan</Nav.Link>
+            <Nav.Link href="/plans">My Plans</Nav.Link>
+            <Nav.Link href="/" onClick={ logout }>Logout</Nav.Link>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto">
+            <Nav.Link href="/signup">Sign Up</Nav.Link>
+            <Nav.Link href="/login">Log In</Nav.Link>
+          </Nav>
+        )
+      }
     </Navbar>
   );
 };
