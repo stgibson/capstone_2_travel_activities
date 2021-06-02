3654d89dee6b04dfda8de9b7c6c72dfd49330fbf
Sean Gibson <seanthomasgibson@gmail.com>
2021-06-01 18:27:23 -0700

    Only doing smoke test for App component. Updated test in frontend/src/App.test.js.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import App from "./App";
+import rootReducer from "./reducers/root";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const store = createStore(rootReducer);
+
+it("renders without crashing", () => {
+  render(
+    <Provider store={ store }><MemoryRouter><App /></MemoryRouter></Provider>
+  );
 });
