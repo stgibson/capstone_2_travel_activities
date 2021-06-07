a4e716f408fe8e30ea523cbdb1c6a1dde0ef1bd3
Sean Gibson <seanthomasgibson@gmail.com>
2021-06-07 15:17:34 -0700

    Made footer stick to bottom. Modified Footer() in frontend/src/Footer.js

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,26 +1,22 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 
 /**
- * Component for displaying acknowledgements for the website
+ * Component for displaying acknowledgements for the website. Uses Navbar to
+ * make footer stick to bottom.
  * @returns JSX code for rendering footer
  */
 const Footer = () => {
   return (
-    <>
-      <div>
-        <small>
-          The information on travel activities in this website is provided by the
-          Tours and Activities API by Amadeus for Developers located at
-          https://developers.amadeus.com/self-service/category/destination-content/api-doc/tours-and-activities/api-reference
-        </small>
-      </div>
-      <div>
-        <small>
-          Furthermore, this site also uses the Geocoding API located at
-          https://developers.google.com/maps/documentation/geocoding/overview
-        </small>
-      </div>
-    </>    
+    <Navbar fixed="bottom">
+      <small>
+        The information on travel activities in this website is provided by the
+        Tours and Activities API by Amadeus for Developers located at
+        https://developers.amadeus.com/self-service/category/destination-content/api-doc/tours-and-activities/api-reference.
+        Furthermore, this site also uses the Geocoding API located at
+        https://developers.google.com/maps/documentation/geocoding/overview
+      </small>
+    </Navbar>    
   );
 };
 
